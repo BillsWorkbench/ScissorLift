cb7d7d5610ea654a0bb4cbc9b3d65276bbe2ac67
Bill Debevc <37317091+BIMThoughts@users.noreply.github.com>
2019-01-05 18:42:41 -0800

    First Version SwitchTest.PY

---
+++ SwitchTest.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #Down Stop
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  #Up Stop
+
+LowerLimit = 'No'
+UpperLimit = 'No'
+
+if GPIO.input(23) == False:
+        LowerLimit = 'Yes'
+
+if GPIO.input(22) == False:
+        UpperLimit = 'Yes'
+
+while LowerLimit == 'No':
+    if GPIO.input(23) == False:
+        LowerLimit = 'Yes'
+    print ('going down')
+    UpperLimit = 'No'
+    print (LowerLimit)
+print ('out')
+
+while UpperLimit == 'No':
+    if GPIO.input(22) == False:
+        UpperLimit = 'Yes'
+    print ('going up')
+    LowerLimit = 'No'
+    print (UpperLimit)
+print ('done')
+
